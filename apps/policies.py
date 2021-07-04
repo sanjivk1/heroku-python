d8dd3e5e796db921aa870f8ecfe5b9fe93128fde
sanjivk1 <kapoor@iit.edu>
2021-07-04 15:26:24 -0500

    Update policies.py

--- apps/policies.py
+++ apps/policies.py
@@ -1,16 +1,12 @@
-import os
 import dash
-from datetime import datetime
-import json
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import dash_daq as daq
-import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
-from dateutil.parser import parse
+
 
 from app import app
 
